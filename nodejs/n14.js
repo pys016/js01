0868e7d783f6b51ab99ab90a9791ed2dc002c0b4
pys016 <pys8451@naver.com>
2024-07-10 14:23:36 +0900

    수정중

--- nodejs/n14.js
+++ nodejs/n14.js
@@ -5,12 +5,21 @@ const app = express();
 const port = 3000;
 
 app.get("/", (rec, res) => {
-  res.send("안녕하세요 express로 만든 서버입니다.");
+  res.send(
+    "<h1>안녕하세요 express로 만든 서버입니다.</h1><h2><a href='/list'>리스트이동</a></h2>"
+  );
 });
+app.use("/list", express.static(__dirname));
 
-app.get("/", (req, res) => {
+app.get("/list", (req, res) => {
   fs.readdir(__dirname, "utf-8", (err, data) => {
-    res.send(data);
+    let list = `<h1>링크를 선택하세요</h1><h2><ul>`;
+    data.forEach(
+      (v) =>
+        (list += `<li><a href="${v}">${v}</a><a href="${v}" style = "text-decoration-line: none;" download>[Download]</a></li>`)
+    );
+    list += `</ul></h2>`;
+    res.send(list);
   });
 });
 
