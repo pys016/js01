98be79a23c965584269e921a09b5a5803645c1ee
pys016 <pys8451@naver.com>
2024-07-10 11:52:08 +0900

    n14 수정중

--- nodejs/n14.js
+++ nodejs/n14.js
@@ -1,1 +1,19 @@
-//안녕하세요.
+const express = require("express");
+const logger = require("morgan");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.get("/", (rec, res) => {
+  res.send("안녕하세요 express로 만든 서버입니다.");
+});
+
+app.get("/", (req, res) => {
+  fs.readdir(__dirname, "utf-8", (err, data) => {
+    res.send(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(port + "서버가 시작되었습니다.");
+});
