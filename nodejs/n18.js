5cb434ec278d62c9d794d9210ab97b88e493dc41
pys016 <pys8451@naver.com>
2024-07-17 09:19:26 +0900

    2024/07/17

--- nodejs/n18.js
+++ nodejs/n18.js
@@ -4,59 +4,60 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   let list = ``;
-  list += `<!DOCTYPE html>`;
-  list += `<html lang="en">`;
-  list += `  <head>`;
-  list += `    <meta charset="UTF-8" />`;
-  list += `    <meta name="viewport" content="width=device-width, initial-scale=1.0" />`;
-  list += `    <title>구구단</title>`;
-  list += `  </head>`;
-  list += `  <style>`;
+  list += `    <!DOCTYPE html>`;
+  list += `<html lang="ko">`;
+  list += `<head>`;
+  list += `    <meta charset="UTF-8">`;
+  list += `    <meta name="viewport" content="width=device-width, initial-scale=1.0">`;
+  list += `    <title>Document</title>`;
+  list += `</head>`;
+  list += `<style>`;
   list += `    table {`;
-  list += `      width: 100%;`;
-  list += `      border-collapse: collapse;`;
-  list += `    }`;
+  list += `        width: 80%;height: 400px;`;
+  list += `        border-collapse: collapse;`;
+  list += `    text-align: center;}`;
   list += `    th,`;
-  list += `    tr,`;
+  list += `    `;
   list += `    td {`;
-  list += `      border: 1px solid #000;`;
-  list += `    }`;
-  list += `  </style>`;
-  list += `  <body><form action="/dan">
-    <h1>구구단</h1>    
-    <select name="gugu">
-        <option value="2">2단</option>
-        <option value="3">3단</option>
-        <option value="4">4단</option>
-        <option value="5">5단</option>
-    </select>
-    <input type="submit" value="확인" />
-    </form>`;
-  list += `    <table>`;
-  list += `      <tr>`;
-  list += `        <th></th>`;
+  list += `        border: 1px solid #000;width:10%;`;
+  list += `        }`;
+  list += `</style>`;
+  list += `<body><h1>구구단</h1>
+            <form action="/">
+                <select name="gugu">
+                    <option value="0">전체</option>`;
   for (let i = 2; i <= 9; i++) {
-    list += `        <th>${i}단</th>`;
+    list += `         <option value="${i}">${i}단</option>`;
   }
-  list += `      </tr>`;
+  list += ` </select><input type="submit" value="확인"></form>`;
+  list += `<table><tr>`;
 
-  for (let j = 1; j <= 9; j++) {
-    list += `        <tr>`;
-    list += `            <td>${j}</td>`;
-    for (let i = 2; i <= 9; i++) {
-      list += `            <td>${i} x ${j} = ${i * j}</td>`;
+  const gugu = req.query.gugu;
+  if (!gugu || gugu == 0) {
+    list += `<th></th>`;
+    for (let k = 2; k <= 9; k++) {
+      list += `<th>${k}단</th>`;
     }
-    list += `      </tr>`;
+    list += `</tr>`;
+
+    for (let j = 1; j <= 9; j++) {
+      list += `<tr><td>${j}</td>`;
+      for (let i = 2; i <= 9; i++) {
+        list += `<td>${i} x ${j} = ${i * j}</td>`;
+      }
+      list += `</tr>`;
+    }
+  } else {
+    list += `<th>${gugu}단</th></tr>`;
+    for (let i = 2; i <= 9; i++) {
+      list += `<td>${gugu} x ${i} = ${gugu * i}</td></tr>`;
+    }
   }
 
   list += `    </table>`;
-  list += `  </body>`;
+  list += `</body>`;
   list += `</html>`;
   res.send(list);
-});
-app.get("/dan", (req, res) => {
-  const gugu = req.query.gugu;
-  console.log(gugu);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
